4535cab67c79aa27b926102a1769b0af6cbc9b58
JEUN_YONGGYU <wjsdydrb123@naver.com>
2023-01-09 19:06:16 +0900

    no message

--- src/main/java/com/sm/main/HC.java
+++ src/main/java/com/sm/main/HC.java
@@ -18,12 +18,7 @@ public class HC extends HttpServlet {
 		DAO_Master.getAllProduct(request);
 		request.setAttribute("contentPage", "jsp/home.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
-<<<<<<< HEAD
-		
 
-=======
->>>>>>> f43475bac38bbcf0666264e095eab639dc8dc320
-		
 
 	}
 
