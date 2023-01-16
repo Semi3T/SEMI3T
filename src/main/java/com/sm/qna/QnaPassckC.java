7df502539119f99363fbf02d6d3617ff71218127
zdzi6373 <zdzi6373@gmail.com>
2023-01-16 20:09:26 +0900

    logincheck, header.jsp 연결

--- src/main/java/com/sm/qna/QnaPassckC.java
+++ src/main/java/com/sm/qna/QnaPassckC.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sm.account.AccountDAO;
+
 @WebServlet("/QnaPassckC")
 public class QnaPassckC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -21,12 +23,16 @@ public class QnaPassckC extends HttpServlet {
 		
 		if (request.getParameter("pw").equals(request.getAttribute("dbpw"))) {
 			DAO_QnA.getQna(request);
+			AccountDAO.loginCheck(request);
+			request.setAttribute("headerPage", "jsp/header.jsp");
 			request.setAttribute("contentPage", "jsp/qna/qnadetail.jsp");
 			request.getRequestDispatcher("index.jsp").forward(request, response);
 		} else {
 			DAO_QnA.getAllqna(request);
 			DAO_QnA.qnaPaging(1, request);
+			AccountDAO.loginCheck(request);
 			request.setAttribute("invalid", "비밀번호가 일치하지 않습니다.");
+			request.setAttribute("headerPage", "jsp/header.jsp");
 			request.setAttribute("contentPage", "jsp/qna/qnaconfirm.jsp");
 			request.getRequestDispatcher("index.jsp").forward(request, response);
 		}
